5cafd6d200c0e022b73da438bf0af623b6e6379e
lyl <869494563@qq.com>
2018-06-27 11:24:28 -0500

    minor

--- read_test_vary_cc.sh
+++ read_test_vary_cc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm $lustre\gf_test/read_test_files/vary_cc.txt
 for cc in $(seq 20)
 do
 	/usr/bin/time -a -o $lustre\gf_test/read_test_files/vary_cc.txt -f "%e" globus-url-copy -vb -f $lustre\gf_test/read_test_files/h2hList.txt
