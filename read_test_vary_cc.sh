ec674506f68f8f10fa3f445ec39c916066c6d990
lyl <869494563@qq.com>
2018-06-27 11:19:26 -0500

    vary cc script

---
+++ read_test_vary_cc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for cc in $(seq 20)
+do
+	/usr/bin/time -a -o $lustre\gf_test/read_test_files/vary_cc.txt -f "%e" globus-url-copy -vb -f $lustre\gf_test/read_test_files/h2hList.txt
+done	
+
+
--- ssh_start_dtn_server.sh
+++ ssh_start_dtn_server.sh
@@ -6,7 +6,8 @@ from_server_folder=/global/cscratch1/sd/yuanlai/gf_test
 to_port=12335
 to_server_folder=/dev/
 
-for nid in 01 02 04 05 06 07 08 09 10
+#for nid in 01 02 04 05 06 07 08 09 10
+for nid in 01 02 04 05
 do
 	ssh -f dtn$nid "globus-gridftp-server -aa -p $from_port -rp $from_server_folder &"
 	ssh -f dtn$nid "globus-gridftp-server -aa -p $to_port -rp $to_server_folder &"
