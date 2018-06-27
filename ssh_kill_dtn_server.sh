3c62872a43fca77c9b9c3ead24df6ba73a6552ca
lyl <869494563@qq.com>
2018-06-27 11:19:26 -0500

    vary cc script

--- ssh_kill_dtn_server.sh
+++ ssh_kill_dtn_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for nid in 01 02 04 05 06 07 08 09 10
+#for nid in 01 02 04 05 06 07 08 09 10
+for nid in 01 02 04 05
 do
 	ssh dtn$nid 'ps -u yuanlai | grep -i globus | awk '\''{print $1}'\''| xargs kill'
 done
