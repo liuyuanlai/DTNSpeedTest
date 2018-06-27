13db15ca073be69bf5ebefb363429e625bd5e357
lyl <869494563@qq.com>
2018-06-27 13:40:56 -0500

    parse read test result

---
+++ parse_read_test_result.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from __future__ import division
+
+path = "/global/homes/y/yuanlai/workspace/DTNSpeedTest/tmp/vary_cc.txt"
+totalSize = 2**40
+with open(path) as fp:
+	for cc in range(1, 21):
+		time = float(fp.readline())
+		speed = totalSize / time
+		print "cc=" + str(cc) + ":	" + "transfer rate=" + str(speed / (2**20))
