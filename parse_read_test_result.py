80e2551d2e03db243b3dae4d73fb27bea4cf3fe7
Yuanlai Liu <yuanlai@dtn01.nersc.gov>
2018-06-28 08:44:46 -0700

    add tmestamp

--- parse_read_test_result.py
+++ parse_read_test_result.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 from __future__ import division
+import matplotlib.pyplot as plt
 
-path = "/global/homes/y/yuanlai/workspace/DTNSpeedTest/tmp/vary_cc.txt"
+#path = "/global/homes/y/yuanlai/workspace/DTNSpeedTest/tmp/vary_cc.txt"
+path = "/global/cscratch1/sd/yuanlai/gf_test/read_test_files/vary_cc.txt"
 totalSize = 2**40
+ccList = range(1, 31)
+tr = []
 with open(path, 'r') as fp:
-	for cc in range(1, 21):
-		time = float(fp.readline().rstrip('\n'))
+	for cc in range(1, 31):
+		timestr = fp.readline().rstrip('\n')
+		#print timestr
+		try:
+			time = float(timestr)
+		except:
+			break
 		speed = totalSize / time
-		print "cc=" + str(cc) + ":	" + "transfer rate=" + str(speed / (2**20))
+		tr.append("%.2f" % speed)
+		print "cc=" + str(cc) + ":	" + "transfer rate=" + str("%.2f" % (speed / (2**20))) + "M/s"
+plt.plot(ccList, tr)
+plt.show()
