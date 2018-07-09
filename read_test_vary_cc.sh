a11eaf0b7afbf4f27957cdf8e6ba6079dfb2d829
Yuanlai Liu <yuanlai@dtn01.nersc.gov>
2018-07-09 11:37:01 -0700

    minor

--- read_test_vary_cc.sh
+++ read_test_vary_cc.sh
@@ -3,15 +3,15 @@
 #rm $lustre\gf_test/read_test_files/vary_cc.txt
 fileTimeStamp=$(date "+%m.%d-%H.%M.%S")
 
-#filePath=${lustre}gf_test/read_test_results/vary_cc_result/${fileTimeStamp}.txt
-filePath=${lustre}gf_test/read_test_results/vary_cc_result/06.28-08.59.52.txt
-#touch $filePath
-for cc in $(seq 31 40)
+#filePath=${lustre}gf_test/read_test_results/vary_cc_result/06.28-20.25.52_night_posix.txt
+filePath=${lustre}gf_test/read_test_results/vary_cc_result/${fileTimeStamp}_night_mpi.txt
+touch $filePath
+for cc in $(seq 96)
 do
 	timeStamp=$(date "+%H.%M.%S")
 	echo $timeStamp >> $filePath
 	echo $cc
-	/usr/bin/time -a -o $filePath -f "%e" globus-url-copy -vb -cc $cc -f ${lustre}gf_test/read_test_files/h2hList.txt
+	/usr/bin/time -a -o $filePath -f "%e" globus-url-copy -q -cc $cc -f ${lustre}gf_test/read_test_files/h2hList.txt
 done	
 
 
