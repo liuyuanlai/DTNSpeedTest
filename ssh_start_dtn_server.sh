c25bd51578f38f858fea63e3ae970ce47e96bac5
lyl <869494563@qq.com>
2018-07-12 11:37:26 -0500

    minor

--- ssh_start_dtn_server.sh
+++ ssh_start_dtn_server.sh
@@ -10,6 +10,6 @@ to_server_folder=/global/project/projectdirs/m2930/lyl
 #for nid in 01 02 04 05 06 07 08 09 10
 for nid in 01 02 04 05 06 07 08 09 10
 do
-	ssh -f dtn$nid "globus-gridftp-server -aa -p $from_port -rp $from_server_folder &"
-	ssh -f dtn$nid "globus-gridftp-server -aa -p $to_port -rp $to_server_folder &"
+	ssh -f dtn$nid "globus-gridftp-server -aa -p $from_port -rp $from_server_folder,$to_server_folder &"
+#	ssh -f dtn$nid "globus-gridftp-server -aa -p $to_port -rp $to_server_folder &"
 done
