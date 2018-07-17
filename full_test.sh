00a88c22bfa6e074964f0f5678f75b501b749c95
Yuanlai Liu <yuanlai@dtn01.nersc.gov>
2018-07-17 07:42:57 -0700

    minor

--- full_test.sh
+++ full_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-./gpfs_read_test_vary_cc.sh
-./gpfs_write_test_vary_cc.sh
-#./l2g_vary_cc.sh
+for i in $(seq 3)
+do	
+	echo "i: ${i}"
+	./gpfs_read_test_vary_cc.sh
+	./gpfs_write_test_vary_cc.sh
+	./l2g_vary_cc.sh
+done
