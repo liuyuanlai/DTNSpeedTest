4e363a9db7f947eaa1bc28829c18d9658047e19b
Yuanlai Liu <yuanlai@miradtn01.mcp.alcf.anl.gov>
2018-07-18 16:37:24 +0000

    minor

--- ssh_kill_iperf_server.sh
+++ ssh_kill_iperf_server.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-p=50050
 
-for i in 01 02 03 04 05 06 07 08 09 10 11 12
+for i in 02 03 04 05 06 07 08 09 10 11 12 01
 do
 	ssh miradtn$i 'ps -u yuanlai | grep -i iperf | awk '\''{print $1}'\''| xargs kill'
 done
