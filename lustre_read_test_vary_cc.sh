73c544c0b3d2f43c802d19ab4a73a21270ca8c90
Yuanlai Liu <yuanlai@dtn01.nersc.gov>
2018-08-15 13:31:41 -0700

    minor

--- lustre_read_test_vary_cc.sh
+++ lustre_read_test_vary_cc.sh
@@ -4,15 +4,16 @@
 fileTimeStamp=$(date "+%m.%d-%H.%M.%S")
 
 #filePath=${lustre}gf_test/read_test_results/vary_cc_result/06.28-20.25.52_night_posix.txt
-filePath=${lustre}gf_test/new_read_test_results_limit_ost/vary_cc_result/${fileTimeStamp}.txt
+filePath=${lustre}gf_test/new_read_test_results_all_ost/vary_cc_result/${fileTimeStamp}.txt
 touch $filePath
-for cc in 4 8 16 32 64 96 128
+#for cc in 4 8 16 32 64 96 128
+for cc in 192 256 384 512 
 do
 	timeStamp=$(date "+%Y.%m.%d-%H.%M.%S")
 	echo $timeStamp >> $filePath
 	echo $cc
 	echo $cc >> $filePath
-	/usr/bin/time -a -o $filePath -f "%e" globus-url-copy -q -cc $cc -f ${lustre}gf_test/new_test_files_limit_ost/toNullList
+	/usr/bin/time -a -o $filePath -f "%e" globus-url-copy -q -cc $cc -f ${lustre}gf_test/new_test_files_all_ost/toNullList
 	timeStamp=$(date "+%Y.%m.%d-%H.%M.%S")
 	echo $timeStamp >> $filePath
 done	
--- mpi_fread_test/mpi_fread_test.c
+++ mpi_fread_test/mpi_fread_test.c
@@ -10,7 +10,10 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 int main(int argc, char** argv) {
   // Initialize the MPI environment. The two arguments to MPI Init are not
@@ -25,6 +28,8 @@ int main(int argc, char** argv) {
   // Get the rank of the process
   int world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  if (world_rank == 0) 
+  	printf("Application start timestamp: %ld\n", time(NULL));
 
   // Get the name of the processor
   char processor_name[MPI_MAX_PROCESSOR_NAME];
@@ -33,8 +38,11 @@ int main(int argc, char** argv) {
 
   // Print off a hello world message
 
-  char file_name[72] = "/global/cscratch1/sd/yuanlai/gf_test/new_test_files_limit_ost/file";
-  int read_size = 224000;
+  //char file_name[70] = "/global/cscratch1/sd/yuanlai/gf_test/new_test_files_all_ost/file";
+  char file_name[80] = "/global/cscratch1/sd/yuanlai/gf_test/new_test_files_limit_ost_smaller/file";
+  //char file_name[63] = "/global/cscratch1/sd/yuanlai/gf_test/read_test_files/file";
+  //char file_name[72];
+  int read_size = 262144;
   int file_id = world_rank;
   char* id_arr[5];
   FILE* file;
@@ -43,22 +51,70 @@ int main(int argc, char** argv) {
   int result;
   if (buffer == NULL) {fputs ("Memory error", stderr); exit (2);}
 
-  while (file_id < 31941) {
+  //clock_t start, end;
+  struct timeval start, end;
+  long long int fopen_time = 0, fread_time = 0, fclose_time = 0, sleep_time = 0;
+  long long int total_read = 0;
+  //clock_t p_start = 0, p_end = 0;
+  struct timeval p_start, p_end;
+  //p_start = clock();
+  gettimeofday(&p_start, NULL);
+  int sleep_count = 0, file_count = 0;
+  while (file_id < 59581) {
+  //while (file_id < 4000) {
+    //strncpy(file_name, file_name_pre, 72);
+    //file_name[64] = '\0';
+    file_name[74] = '\0';
+    //file_name[57] = '\0';
     sprintf(id_arr, "%ld", file_id);
     strcat(file_name, id_arr);
+    //start = clock();
+    file_count++;
+    //printf("reading file: %s on node: %s\n", file_name, processor_name);
+    gettimeofday(&start, NULL);
     file = fopen(file_name, "rb");
-    if (!file) {fputs ("File open error: %s", file_name, stderr); exit(1);}
-
+    //end = clock();
+    gettimeofday(&end ,NULL);
+    //fopen_time += ((double) (end - start)) / CLOCKS_PER_SEC;
+    fopen_time += ((end.tv_sec - start.tv_sec) * 1000000) + (end.tv_usec - start.tv_usec);
+    if (!file) {printf("file open error: %s", file_name); exit(1);}
+    
+ 
     result = read_size;
     while (result == read_size) {
+      //start = clock();
+      gettimeofday(&start, NULL);
       result = fread(buffer, 1, read_size, file);
-      printf("Read %d bytes from processor %s, rank %d out of %d processors\n",
-           result, processor_name, world_rank, world_size);
+      //end = clock();
+      gettimeofday(&end ,NULL);
+      //fread_time += ((double) (end - start)) / CLOCKS_PER_SEC;
+      fread_time += ((end.tv_sec - start.tv_sec) * 1000000) + (end.tv_usec - start.tv_usec);
+      total_read += result;
+      gettimeofday(&start, NULL);
+      usleep(1500);
+      gettimeofday(&end ,NULL);
+      sleep_time += ((end.tv_sec - start.tv_sec) * 1000000) + (end.tv_usec - start.tv_usec);
+      sleep_count++;
     }
+    gettimeofday(&start, NULL);
     fclose(file);
+    gettimeofday(&end ,NULL);
+    fclose_time += ((end.tv_sec - start.tv_sec) * 1000000) + (end.tv_usec - start.tv_usec);
     file_id += 10;
   }
-
+  //p_end = clock();
+  gettimeofday(&p_end ,NULL);
+  //double total_time = ((double) (p_end - p_start)) / CLOCKS_PER_SEC;
+  long long int total_time = ((p_end.tv_sec - p_start.tv_sec) * 1000000) + (p_end.tv_usec - p_start.tv_usec);
+  printf("processor on node %s, rank %d out of %d processors:\n",
+            processor_name, world_rank, world_size);
+  long long int missing_time = total_time - fopen_time - fread_time - fclose_time - sleep_time;
+  printf("Total open file time: %lldus\nTotal read file time: %lldus\nTotal close file time: %lldus\nSleep time: %lldus\nTotal elapsed time: %lldus\nMissing time:%lldus\nTotal bytes readed: %lld\nTotal files readed: %d\nTotal sleep counts: %d\n", 
+            fopen_time, fread_time, fclose_time, sleep_time, total_time, missing_time, total_read, file_count, sleep_count);
+  printf("=================================================================\n");
+  MPI_Barrier(MPI_COMM_WORLD);
+  if (world_rank == 0) 
+  	printf("Application end timestamp: %ld\n", time(NULL));
   // Finalize the MPI environment. No more MPI calls can be made after this
   MPI_Finalize();
 }
