50c118897e0d15d80c8534316ef8bf36b95232a7
Yuanlai Liu <yuanlai@dtn01.nersc.gov>
2018-08-16 15:20:21 -0700

    minor

--- gpfs_mpi_fread_test.sh
+++ gpfs_mpi_fread_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-file="mpi_fread_test/gpfs_vary_np_test1.txt"
+file="mpi_fread_test/gpfs/gpfs_vary_np_test1.txt"
 
 for np in 1 2 4 6 8 10 12 14 16 18 20
 do
 	echo $np
 	echo "np: ${np}" >> $file
-	mpirun -n $np -f mpi_fread_test/host_file mpi_fread_test/gpfs_sao_mpi_fread_test >> $file
-	mpirun -n $np -f mpi_fread_test/host_file mpi_fread_test/gpfs_bao_mpi_fread_test >> $file
+	mpirun -n $np -f mpi_fread_test/host_file mpi_fread_test/gpfs_sao_mpi_fread_test $np >> $file
+	mpirun -n $np -f mpi_fread_test/host_file mpi_fread_test/gpfs_bao_mpi_fread_test $np >> $file
 done	
 echo "done"
 
--- lustre_mpi_fread_test.sh
+++ lustre_mpi_fread_test.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-file="mpi_fread_test/vary_np_test_new1.txt"
+file="mpi_fread_test/lustre/lustre_vary_np_test1.txt"
 
 for np in 10 20 40 60 80 100 200 400 600 800
 do
 	echo $np
 	echo "np: ${np}" >> $file
-	mpirun -n $np -f mpi_fread_test/host_file mpi_fread_test/s5o_mpi_fread_test >> $file
-	mpirun -n $np -f mpi_fread_test/host_file mpi_fread_test/sao_mpi_fread_test >> $file
-	mpirun -n $np -f mpi_fread_test/host_file mpi_fread_test/bao_mpi_fread_test >> $file
+	mpirun -n $np -f mpi_fread_test/host_file mpi_fread_test/lustre_sao_mpi_fread_test $np >> $file
+	mpirun -n $np -f mpi_fread_test/host_file mpi_fread_test/lustre_bao_mpi_fread_test $np >> $file
 done	
 echo "done"
 
