08b30f076a19edc076d4f78484e3873425d94d66
Yuanlai Liu <yuanlai@miradtn01.mcp.alcf.anl.gov>
2018-08-21 15:17:47 +0000

    minor

--- mira_mpi_fread_test.sh
+++ mira_mpi_fread_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-file="mpi_fread_test/mira/mira_vary_np_test_no_sleep1.txt"
+file="mpi_fread_test/mira_dynamic/mira_dynamic_vary_np_test.txt"
 
-for np in 12 24 48 72 96 120 240 480 720 960
+for np in 13 25 49 73 97 121 241 481 721 961
 do
 	echo $np
 	echo "np: ${np}" >> $file
-	mpirun -n $np -f mpi_fread_test/mira_host_file mpi_fread_test/mira_sao_mpi_fread_test $np >> $file
-	mpirun -n $np -f mpi_fread_test/mira_host_file mpi_fread_test/mira_bao_mpi_fread_test $np >> $file
+	mpirun -n $np -f mpi_fread_test/mira_host_file mpi_fread_test/dynamic_mpi_fread_test mpi_fread_test/s_fname_list.txt >> $file
+	mpirun -n $np -f mpi_fread_test/mira_host_file mpi_fread_test/dynamic_mpi_fread_test mpi_fread_test/b_fname_list.tx t>> $file
 done	
 echo "done"
 
