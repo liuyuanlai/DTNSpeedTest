ce65094680814a91598fca9ad8329f81de559a16
Yuanlai Liu <yuanlai@dtn01.nersc.gov>
2018-08-30 20:46:03 -0700

    minor

--- mpi_fread_test/dy_OST_verify.cpp
+++ mpi_fread_test/dy_OST_verify.cpp
@@ -11,6 +11,7 @@
 #include <string>
 #include <algorithm>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -51,6 +52,7 @@ int main(int argc, char** argv) {
     MPI_Get_processor_name(processor_name, &name_len);
 
     vector<vector<pair<long int, string>>> v_file_ost(OST_NUM);
+    unordered_map<string, int> path_ost;
 
     if (rank == 0) {
         fp = fopen(argv[1], "r");
@@ -61,21 +63,27 @@ int main(int argc, char** argv) {
 
         char fname[MAX_FNAME_LEN], fsize[12], fost[10];
         string f_name;
-        long int f_size; 
+        long int f_size;
         int f_ost;
 
         while (fscanf(fp, "%s %s %s", fname, fsize, fost) != EOF) {
             f_name = fname; 
             //f_size = atoi(fsize);
+            //memcpy (&f_size, fsize, sizeof(long));
             f_size = strtol (fsize, NULL, 10);
             f_ost = atoi(fost);
             // cout << f_name << " " << f_size << " " << f_ost << endl;
             v_file_ost[f_ost].push_back({f_size, f_name});
+            path_ost[f_name] = f_ost;
         }
 
         for (int i = 0; i < OST_NUM; i++)
             sort(v_file_ost[i].begin(), v_file_ost[i].end(), [](const pair<long int, string>& a, 
                 const pair<long int, string>& b) {return a.first > b.first; });
+        for (int j = 0; j < OST_NUM; j++)
+            for (auto p : v_file_ost[j]) {
+                cout << p.second << " " << p.first << endl;
+            }
     }
 
 
@@ -84,11 +92,13 @@ int main(int argc, char** argv) {
 
     // rank 0 works as the scheduler
     if (rank == 0) {
+	ofstream outputfile("veryfy_log.txt");
 
         vector<int> ost_pointer(OST_NUM, 0);
         vector<int> ost_count(OST_NUM, 0);
         unordered_map<int, int> rank_ost;
         vector<bool> ost_completed(OST_NUM, false);
+
         int next_ost = 0;
         string f_name;
         for (int r = 1; r < np; r++) {
@@ -115,13 +125,19 @@ int main(int argc, char** argv) {
 
             if (next_ost >= 0) {
                 f_name = v_file_ost[next_ost][ost_pointer[next_ost]++].second;
-                if (ost_pointer[next_ost] == v_file_ost[next_ost].size()) ost_completed[next_ost] = true;
+                if (ost_pointer[next_ost] == v_file_ost[next_ost].size()) {
+                    ost_completed[next_ost] = true;
+                    outputfile << next_ost << " completed!" << endl;
+                }
                 rank_ost[req_rank] = next_ost;
                 ost_count[next_ost] += 1;
 
                 strcpy(file_name, f_name.c_str());
                 ret = MPI_Send(&file_name, MAX_FNAME_LEN, MPI_CHAR, req_rank, 0, MPI_COMM_WORLD);
-
+		
+                outputfile << f_name << " " << path_ost[f_name] << " " << req_rank << "\n";
+                for (auto i : ost_count) outputfile << i << " ";
+                outputfile << "\n";
                 // printf("Assign reading task: %s to %d\n", file_name, req_rank);
 
             }
@@ -141,8 +157,7 @@ int main(int argc, char** argv) {
         fclose(fp);
     }else{
         struct timeval start, end, g_start, g_end;
-        long long tfopen = 0, tfread = 0, tfclose = 0, tsleep = 0, telapse = 0, cfread = 0, nfile = 0, nbyte = 0, rtsleep = 0;;
-        long long t_per_fread;
+        long long tfopen = 0, tfread = 0, tfclose = 0, tsleep = 0, telapse = 0, cfread = 0, nfile = 0, nbyte = 0;
         gettimeofday(&g_start, NULL);
         ret = MPI_Recv(file_name, MAX_FNAME_LEN, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         while(1){
@@ -154,8 +169,8 @@ int main(int argc, char** argv) {
                 gettimeofday(&g_end, NULL);
                 telapse = ((g_end.tv_sec - g_start.tv_sec) * S2MICRO_S) + (g_end.tv_usec - g_start.tv_usec);
                 // printf("%d received a finishing signal\n", rank);
-                snprintf(log_buffer, MAX_LOG_LEN, "%d,%d,%s,%lld,%lld,%lld,%lld,%lld,%lld,%lld,%lld,%lld\n", 
-                         np-1, rank-1, processor_name, nfile, nbyte, tfopen, tfread, tfclose, tsleep, telapse, cfread, rtsleep);
+                snprintf(log_buffer, MAX_LOG_LEN, "%d,%d,%s,%lld,%lld,%lld,%lld,%lld,%lld,%lld,%lld\n", 
+                         np-1, rank-1, processor_name, nfile, nbyte, tfopen, tfread, tfclose, tsleep, telapse, cfread);
 
                 ret = MPI_Send(log_buffer, MAX_LOG_LEN, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
                 break;
@@ -176,21 +191,17 @@ int main(int argc, char** argv) {
                 gettimeofday(&start, NULL);
                 rd_size = fread(io_buffer, 1, BLOCK_SIZE, fp);
                 gettimeofday(&end, NULL);
-                t_per_fread = ((end.tv_sec - start.tv_sec) * S2MICRO_S) + (end.tv_usec - start.tv_usec);
-                tfread += t_per_fread; 
+                tfread += ((end.tv_sec - start.tv_sec) * S2MICRO_S) + (end.tv_usec - start.tv_usec);
                 nbyte  += rd_size;
                 cfread ++;
                 if (rd_size != BLOCK_SIZE){
                     break;
                 }
-                //long long _tsleep = net_wr_us > t_per_fread ? net_wr_us - t_per_fread: 0;
-                long long _tsleep = 0;
+
                 gettimeofday(&start, NULL);
-                //usleep(_tsleep);
-                //usleep(0);
+                usleep(net_wr_us);
                 gettimeofday(&end, NULL);
-                rtsleep += _tsleep;
-                tsleep  += ((end.tv_sec - start.tv_sec) * S2MICRO_S) + (end.tv_usec - start.tv_usec);            
+                tsleep += ((end.tv_sec - start.tv_sec) * S2MICRO_S) + (end.tv_usec - start.tv_usec);            
             }
             gettimeofday(&start, NULL);
             fclose(fp);
--- mpi_fread_test/fio-emu4ftrs.cpp
+++ mpi_fread_test/fio-emu4ftrs.cpp
@@ -30,6 +30,7 @@ int main(int argc, char** argv) {
     size_t rd_size;
     MPI_Get_processor_name(processor_name, &name_len);
 
+    int net_wr_us = (int)(BLOCK_SIZE / (1e10 / (np - 1)) * S2MICRO_S); // network write time, asssue 80Gbps
     // rank 0 works as the scheduler
     char* tmp_rdbuf = NULL;
     if (rank == 0) {
@@ -37,6 +38,13 @@ int main(int argc, char** argv) {
         if (!fp) {
             printf("Cannot open file of file name\n"); 
             exit(0);
+        }
+        for(int r=1; r<np; r++){
+            if (getline(&tmp_rdbuf, &rd_size, fp) != -1){ // use fscanf if ost information is provided
+                tmp_rdbuf[strlen(tmp_rdbuf)-1] = '\0'; // remove CR
+                strcpy(file_name, tmp_rdbuf);
+                ret = MPI_Send(&file_name, MAX_FNAME_LEN, MPI_CHAR, r, 0, MPI_COMM_WORLD);
+            }            
         }
         // printf("start experiment with np:%d\n", np);
         while (1){
@@ -62,20 +70,20 @@ int main(int argc, char** argv) {
         fclose(fp);
     }else{
         struct timeval start, end, g_start, g_end;
-        long long tfopen = 0, tfread = 0, tfclose = 0, tsleep = 0, telapse = 0, cfread = 0, nfile = 0, nbyte = 0;
+        long long tfopen = 0, tfread = 0, tfclose = 0, tsleep = 0, telapse = 0, cfread = 0, nfile = 0, nbyte = 0, rtsleep = 0;
+        long long t_per_fread; 
         gettimeofday(&g_start, NULL);
+        ret = MPI_Recv(file_name, MAX_FNAME_LEN, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         while(1){
             // send a dummy to request a file from scheduler
             // printf("%d sent request to scheduler\n", rank);
-            ret = MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-            ret = MPI_Recv(file_name, MAX_FNAME_LEN, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             // send log to rank 0 when there is no more job
             if (strlen(file_name) == 0){
                 gettimeofday(&g_end, NULL);
                 telapse = ((g_end.tv_sec - g_start.tv_sec) * S2MICRO_S) + (g_end.tv_usec - g_start.tv_usec);
                 // printf("%d received a finishing signal\n", rank);
-                snprintf(log_buffer, MAX_LOG_LEN, "%d,%d,%s,%lld,%lld,%lld,%lld,%lld,%lld,%lld,%lld\n", 
-                         np-1, rank-1, processor_name, nfile, nbyte, tfopen, tfread, tfclose, tsleep, telapse, cfread);
+                snprintf(log_buffer, MAX_LOG_LEN, "%d,%d,%s,%lld,%lld,%lld,%lld,%lld,%lld,%lld,%lld,%lld\n", 
+                         np-1, rank-1, processor_name, nfile, nbyte, tfopen, tfread, tfclose, tsleep, telapse, cfread, rtsleep);
 
                 ret = MPI_Send(log_buffer, MAX_LOG_LEN, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
                 break;
@@ -96,22 +104,29 @@ int main(int argc, char** argv) {
                 gettimeofday(&start, NULL);
                 rd_size = fread(io_buffer, 1, BLOCK_SIZE, fp);
                 gettimeofday(&end, NULL);
-                tfread += ((end.tv_sec - start.tv_sec) * S2MICRO_S) + (end.tv_usec - start.tv_usec);
+                t_per_fread = ((end.tv_sec - start.tv_sec) * S2MICRO_S) + (end.tv_usec - start.tv_usec);
+                tfread += t_per_fread; 
                 nbyte  += rd_size;
                 cfread ++;
                 if (rd_size != BLOCK_SIZE){
                     break;
                 }
-
+                //long long _tsleep = net_wr_us > t_per_fread ? net_wr_us - t_per_fread: 0;
+                long long _tsleep = 0;
                 gettimeofday(&start, NULL);
-                usleep(0);
+                //usleep(_tsleep);
+                //usleep(0);
                 gettimeofday(&end, NULL);
-                tsleep += ((end.tv_sec - start.tv_sec) * S2MICRO_S) + (end.tv_usec - start.tv_usec);            
+                rtsleep += _tsleep;
+                tsleep  += ((end.tv_sec - start.tv_sec) * S2MICRO_S) + (end.tv_usec - start.tv_usec);            
             }
             gettimeofday(&start, NULL);
             fclose(fp);
             gettimeofday(&end, NULL);
             tfclose += ((end.tv_sec - start.tv_sec) * S2MICRO_S) + (end.tv_usec - start.tv_usec);
+
+            ret = MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+            ret = MPI_Recv(file_name, MAX_FNAME_LEN, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
     }
     MPI_Barrier(MPI_COMM_WORLD);
