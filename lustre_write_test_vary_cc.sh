a961f62043762b717ee568cc70f4590c69c8785a
Yuanlai Liu <yuanlai@dtn01.nersc.gov>
2018-09-02 11:50:03 -0700

    minor

--- lustre_write_test_vary_cc.sh
+++ lustre_write_test_vary_cc.sh
@@ -6,10 +6,10 @@ n2gDir=${lustre}gf_test/write_test_results/
 
 rm -rf $n2gDir
 
-filePath=${lustre}gf_test/write_test_files/${fileTimeStamp}_posix.txt
+filePath=${lustre}gf_test/write_test_files/${fileTimeStamp}.txt
 touch $filePath
 t=60
-for cc in $(seq 2 2 96)
+for cc in 1 2 4 8 10 20 30 40 50 60 70 80 90 100 200 300 400
 do
 	mkdir $n2gDir
 	timeStamp=$(date "+%Y-%m-%d %H.%M.%S")
--- start_iperf_nersc2alcf.sh
+++ start_iperf_nersc2alcf.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-fileTimeStamp=$(date "+%Y.%m.%d-%H.%M.%S")
+fileTimeStamp=$(date -u +%s)
 
 filePath=${lustre}gf_test/iperf_nersc2alcf_test_results/${fileTimeStamp}.txt
 touch filePath
 echo $fileTimeStamp >> $filePath
-echo "t=300" >> $filePath
+echo "t=600" >> $filePath
 echo "p=10" >> $filePath
 ./iperf_nersc2alcf.sh >> $filePath
 
