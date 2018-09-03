35fbfe9f03e6e7b5852b7e6aad709984a7cc0ecb
Yuanlai Liu <yuanlai@dtn01.nersc.gov>
2018-09-02 21:31:59 -0700

    minor

--- mpi_fwrite_test/fwrite-test.sh
+++ mpi_fwrite_test/fwrite-test.sh
@@ -4,5 +4,5 @@ echo "np,rank,dtn,nfile,nbyte,tfopen,tfwrite,tfclose,tsleep,telapse,cfwrite,rtsl
 
 for np in 2 3 5 9 11 21 31 41 51 61 71 81 91 101 201 301 401
 do
-	mpirun -np $np -f host_file ./a.out small5fsize-bysize.txt >> nosleep-small248fns-bysize.csv
+	mpirun -np $np -f host_file ./a.out smallfsize-bysize.txt >> nosleep-small248fns-bysize.csv
 done
