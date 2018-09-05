dd30ec080f0ce29ee253650c6b69ca9fa9e4e41e
Yuanlai Liu <yuanlai@dtn01.nersc.gov>
2018-09-05 14:57:29 -0700

    minor

--- ssh_start_nersc_globus_server.sh
+++ ssh_start_nersc_globus_server.sh
@@ -11,6 +11,6 @@ to_server_folder=/global/project/projectdirs/m2930/lyl
 for nid in 01 02 03 04 05 06 07 08 09 10
 do
 	#ssh -f dtn$nid "globus-gridftp-server -aa -p $from_port -rp $from_server_folder,$to_server_folder,/dev &"
-	ssh -f dtn$nid "globus-gridftp-server -aa -p $from_port -rp $from_server_folder,/dev/zero,/dev/null &"
+	ssh -f dtn$nid "globus-gridftp-server -aa -p $from_port -rp $from_server_folder,$to_server_folder,/dev/zero,/dev/null &"
 	#ssh -f dtn$nid "globus-gridftp-server -aa -p $to_port -rp /dev &"
 done
